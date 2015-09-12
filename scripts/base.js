f5c17bf970ad2135a6185b9b03dddc3b49945433
Mike Chen <chintown@gmail.com>
2015-09-11 20:26:04 -0700

    Fix factory id
    only refer to Model's prototype to make sure it's uniqueness

--- scripts/base.js
+++ scripts/base.js
@@ -72,7 +72,7 @@
   var Model = Class.extend({
     'factoryId': 0,
     'init': function(controller) {
-      this.mId = ++this.__proto__.factoryId;
+      this.mId = ++Model.prototype.factoryId;
       this.mController = controller || this; // logic might be held by upstream
       this.m$dom = null;
       this.m$domParent = null;
